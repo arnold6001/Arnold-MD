9694c4cbb35b582111941a5bb408243d2e51770b
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-09-12 12:25:28 +0300

    Add files via upload

---
+++ talkdove.js
@@ -0,0 +1,31 @@
+
+{
+"bot-name": "Arnold MD",
+  "description": "Made By Arnold Chirchir",
+  "logo": "https://url.bwmxmd.online/Adams.0dhfcjpi.jpeg",
+  "keywords": ["bot"],
+  "documentation-link": "https://session-v35f.onrender.com/",
+    "owner-verification":"arnoldkipruto193@gmail.com",
+  
+    "env": {
+      "SESSION_ID": {
+        "description": "Put your SESSION_ID here. It starts with malvin~",
+        "value":"",
+        "required": true
+        
+      },
+      
+    "OWNER_NUMBER": {
+      "description": "Enter owner number",
+      "required": true,
+      "value": "254703110780"
+      
+       },
+      "TIMEZONE": {
+      "description": "Enter country timezone",
+      "required": true,
+      "value": "Africa/Nairobi"
+
+    }  
+  }
+}
